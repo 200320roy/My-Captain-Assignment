9759cbbafa4adbdf8f4d88105fca4934e9d006d5
Roy20 <104708036+200320roy@users.noreply.github.com>
2022-05-06 12:57:19 +0530

    Add files via upload

---
+++ project 1.1.py
@@ -0,0 +1,52 @@
+print("Hello, World");
+
+if 17>10:
+    print("Seventeen");
+
+    
+#String Concatenation
+
+x="I AM A"
+Y="CODER"
+print(x+Y)
+
+#addition
+x=20.13
+y=34
+print(x+y)
+
+
+"""arithmetic operators
+/=deciml value,//=integer value"""
+
+18/3
+18//3
+
+#assignment operators
+"""= means assignment and == means comparison"""
+
+a=5
+print(a)
+
+a+=5  #=+(shorthand) means a=a+5
+print(a)
+
+
+myTuple=("C","C++")
+print(myTuple);
+
+
+
+
+myDict={
+    "c":"Easy",
+    "C++":"Moderate",
+    "Java":"Tough"
+    }
+myDict["PHP"]="web"
+print(myDict)
+myDict.pop("c")
+print(myDict)
+del myDict["Java"]
+print(myDict)
+    
